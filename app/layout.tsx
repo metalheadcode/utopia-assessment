085a1f08935fcbee95bfa438a4def9613f6dabbf
Ahmad Hazim <metalheadcoder@gmail.com>
2025-07-17 14:13:00 +0800

    Add Open Graph meta tags and image for improved SEO: Include essential Open Graph metadata in the layout for better social media sharing and visibility. Add a new image asset for the Open Graph representation, enhancing the overall branding of the SejookNamastey application.

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,8 @@ import { AuthProvider } from "./context/auth-context";
 import { EmailLinkHandler } from "@/components/email-link-handler";
 import { WhatsappProvider } from "./context/whatsapp";
 
+
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -28,6 +30,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <meta property="og:image" content="/images/og.jpeg" />
+      <meta property="og:title" content="SejookNamastey" />
+      <meta property="og:description" content="SejookNamastey: Dashboard for Sejuk Sejuk Services Sdn Bhd, made by @metalheadcode" />
+      <meta property="og:url" content="https://utopia-assessment.vercel.app" />
+      <meta property="og:type" content="website" />
+      <meta property="og:image:type" content="image/jpeg" />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="720" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
